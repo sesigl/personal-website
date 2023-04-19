796147dc08fb5609f36f6eb0d40987d02842ab44
Sebastian Sigl <sebastian.sigl@adevinta.com>
2023-04-19 20:49:53 +0200

    Fix filter item category type error

--- src/app/home/[category]/FilterItem.tsx
+++ src/app/home/[category]/FilterItem.tsx
@@ -13,7 +13,7 @@ export default function FilterItem({
     ? "block py-3 font-medium text-slate-800 dark:text-slate-100 border-b-2 border-sky-500"
     : "block py-3 text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300";
 
-  let href = value === "Backend" ? "/" : `/home/${value}`;
+  let href = value === "All" ? "/" : `/home/${value}`;
   return (
     <li className="px-3 -mb-px">
       <Link className={aClassNames} href={href as any}>
