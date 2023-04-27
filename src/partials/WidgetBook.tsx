7f55a6da14417dcab4437328e39257ca669d7f93
Sebastian Sigl <sebastian.sigl@adevinta.com>
2023-04-27 07:12:44 +0200

    Add link to notion templates

--- src/partials/WidgetBook.tsx
+++ src/partials/WidgetBook.tsx
@@ -4,18 +4,20 @@ import Image from "next/image";
 function WidgetBook() {
   return (
     <div className="rounded-lg border border-slate-200 dark:border-slate-800 dark:bg-gradient-to-t dark:from-slate-800 dark:to-slate-800/30 odd:rotate-1 even:-rotate-1 p-5">
-      <div className="font-aspekta font-[650] text-center mb-3">
-        Free E-Book
-      </div>
-      <div className="text-center">
-        <Image
-          className="inline-flex rounded-lg shadow-lg rotate-3"
-          src={"/images/book.png"}
-          width="148"
-          height="190"
-          alt="Book"
-        />
-      </div>
+      <a href="https://sesigl.gumroad.com/" target="_blank">
+        <div className="font-aspekta font-[650] text-center mb-3">
+          Free Notion Templates
+        </div>
+        <div className="text-center">
+          <Image
+            className="inline-flex rounded-lg shadow-lg rotate-3"
+            src={"/images/book-2.png"}
+            width="148"
+            height="190"
+            alt="Book"
+          />
+        </div>
+      </a>
     </div>
   );
 }
