9d140a7fea247bfa9e7d7b4604c1c2939cbef04d
Sebastian Sigl <akrillo89@gmail.com>
2023-05-02 18:22:18 +0200

    Revert "Update robots txt to ignore urls that cause google search warns"
    This reverts commit 53157811e5ce670f9463a571d350ba26784c8b5b.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://www.sebastiansigl.com",
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: "*",
-        disallow: [
-          "https://sebastiansigl.com",
-          "http://sebastiansigl.com",
-          "http://www.sebastiansigl.com",
-        ],
-      },
-    ],
-  },
-};
+    siteUrl: process.env.SITE_URL || 'https://www.sebastiansigl.com',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
