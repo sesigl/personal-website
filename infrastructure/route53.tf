abf4bfa25f4107e034949d43aeb08da4f49256ef
Sebastian Sigl <sebastian.sigl@adevinta.com>
2023-05-24 07:01:13 +0200

    Setup icloud for private mails

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -23,5 +23,21 @@ resource "aws_route53_record" "google_verification_txt" {
   name    = ""
   type    = "TXT"
   ttl     = 300
-  records = ["google-site-verification=XPyaSlZabo5ucYd56L0VZd6-hFbyiQ0-lK0p3AxsxTA"]
+  records = ["google-site-verification=XPyaSlZabo5ucYd56L0VZd6-hFbyiQ0-lK0p3AxsxTA", "apple-domain=XMFMGL9LHP5pJW20", "v=spf1 include:icloud.com ~all"]
+}
+
+resource "aws_route53_record" "icloud_verification_mx" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = ""
+  type    = "MX"
+  ttl     = 300
+  records = ["mx01.mail.icloud.com.", "mx02.mail.icloud.com."]
+}
+
+resource "aws_route53_record" "cname_icloud" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "sig1._domainkey"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["sig1.dkim.sebastiansigl.com.at.icloudmailadmin.com."]
 }
